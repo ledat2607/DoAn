429b26978219903cb9eefd81ab87d4e2abd615bb
LAPTOP-PPVTJ3FP\ADMIN <leluuquocdat9@gmail.com>
2023-10-25 20:38:13 +0700

    first 25_10

--- frontend/src/components/Shop/AllProducts.jsx
+++ frontend/src/components/Shop/AllProducts.jsx
@@ -20,39 +20,63 @@ const AllProducts = () => {
   }
   return (
     <div className="w-full">
-      <div className="w-[95%] h-[140px] p-4 rounded-lg mx-auto shadow-xl bg-gray-300">
-        {products &&
-          products.map((i, index) => (
-            <div className="flex justify-between items-center">
-              <div>
-                <p>Hình ảnh sản phẩm</p>
-                <img
-                  src={`${backend_url}${i.images[0]}`}
-                  className="w-[120px] h-[80px] rounded-md"
+      {products &&
+        products.map((i, index) => (
+          <div className="flex justify-between mx-auto w-[95%] bg-white shadow-2xl p-4 rounded-md mt-6 800px:h-[150px] h-[120px]">
+            <div className="w-[120px] relative">
+              <span className="800xp:text-md text-[12px]">Hình ảnh</span>
+              <img
+                src={`${backend_url}${i.images[0]}`}
+                className="800px:w-[120px] w-[80px] hover:cursor-pointer pt-2"
+              />
+              <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center transition-opacity duration-300">
+                <AiOutlineDelete
+                  size={20}
+                  color="red"
+                  className="cursor-pointer hover:scale-[1.2] text-white"
+                />
+                <AiOutlineEdit
+                  size={20}
+                  color="green"
+                  className="cursor-pointer hover:scale-[1.2] text-white ml-4"
                 />
               </div>
-              <div>
-                <p>Tên sản phẩm</p>
-                <span className="mt-10">{i.name}</span>
-              </div>
-              <div>
-                <p>Giá gốc</p>
-                <span>{formatVietnameseCurrency(i.originalPrice)}</span>
-              </div>
-              <div>
-                <p>Giá khuyến mãi</p>
-                <span>{formatVietnameseCurrency(i.discountPrice)}</span>
-              </div>
-              <div>
-                <p>Chức năng</p>
-                <div className="flex justify-between">
-                  <AiOutlineDelete className="cursor-pointer" size={20} />
-                  <AiOutlineEdit className="cursor-pointer" size={20} />
-                </div>
+            </div>
+            <div className="w-[150px]">
+              <span className="800xp:text-md text-[12px]">Tên sản phẩm</span>
+              <div className="800xp:text-md text-[12px] text-blue-500 mt-6">
+                {i.name}
               </div>
             </div>
-          ))}
-      </div>
+            <div>
+              <span className="hidden 800px:block 800xp:text-md">Giá gốc</span>
+              <div className="text-md text-red-500 800px:block hidden mt-6">
+                {formatVietnameseCurrency(i.originalPrice)}
+              </div>
+            </div>
+            <div>
+              <span className="800xp:text-md text-[12px]">Giá bán</span>
+              <div className="800xp:text-md text-[12px] text-green-500 mt-6">
+                {formatVietnameseCurrency(i.discountPrice)}
+              </div>
+            </div>
+            <div className="hidden 800px:block">
+              <span>Chức năng</span>
+              <div className="flex justify-between mt-6">
+                <AiOutlineDelete
+                  size={20}
+                  color="red"
+                  className="cursor-pointer hover:scale-[1.2]"
+                />
+                <AiOutlineEdit
+                  size={20}
+                  color="green"
+                  className="cursor-pointer hover:scale-[1.2]"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
--- frontend/src/pages/Shop/ShopAllProduct.jsx
+++ frontend/src/pages/Shop/ShopAllProduct.jsx
@@ -5,13 +5,13 @@ import AllProducts from "../../components/Shop/AllProducts";
 
 const ShopAllProduct = () => {
   return (
-    <div>
+    <div className="overflow-y-scroll">
       <DashboardHeader />
       <div className="flex w-full">
-        <div className="800px:w-[25%] w-[10%]">
+        <div className="800px:w-[17%] w-[12%]">
           <DashboardSideBar active={3} />
         </div>
-        <div className="w-[85%] mx-auto mt-10">
+        <div className="w-[83%] mx-auto mt-10">
           <AllProducts />
         </div>
       </div>
